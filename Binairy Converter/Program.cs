99d4bfe41d76a7f44b48d4ed00c53663d0873f63
X <147497059+XanderZKFH9FH@users.noreply.github.com>
2025-11-28 09:43:21 +0100

    Add files via upload

---
+++ Binairy Converter/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Binairy_Converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Vars
+
+            int BinairyNumber = 0;
+            string BinairySolution = "";
+            int Length = 0;
+            string Num = "";
+            int Loops = 0;
+            int Max = 1;
+            int Pow = 0;
+            int Output = 0;
+            int UserBreak = 0;
+            while (UserBreak != 1)
+            {
+                //End
+
+                Console.WriteLine("Type 1 to quit converting");
+                int.TryParse(Console.ReadLine(), out UserBreak);
+
+                //BreakCheck
+                if (UserBreak != 1)
+                {
+
+                    //TXT
+
+                    Console.WriteLine("Hey, I am asuming you want to convert a binairy number (up to 8 bits)!");
+                    Console.Write("Please insert your binairy number : ");
+
+                    // Conversions
+
+                    int.TryParse(Console.ReadLine(), out BinairyNumber);
+                    Console.WriteLine(BinairyNumber);
+                    BinairySolution = Convert.ToString(BinairyNumber);
+                    Length = BinairySolution.Length;
+
+                    // Calc
+
+                    while (Length != 0)
+                    {
+                        Num = BinairySolution.Substring(Loops, Max);
+                        Length--;
+                        if (Num == "1")
+                        {
+                            Output = (int)(Output + Math.Pow(2, Loops));
+                        }
+                        else
+                        {
+
+                        }
+                        Loops++;
+                    }
+                    Console.WriteLine(Output);
+                }
+            }
+        }
+    }
+}
